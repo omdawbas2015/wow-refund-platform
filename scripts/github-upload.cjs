4c985e6e3ddaf2c2671e3a3aa73b45c4fc53baa6
Repo Automator <noreply@example.com>
2026-04-25 05:04:41 +0300

    Initial commit

--- scripts/github-upload.cjs
+++ scripts/github-upload.cjs
@@ -2,11 +2,154 @@ const fs = require('fs');
 const fsp = require('fs').promises;
 const path = require('path');
 const git = require('isomorphic-git');
-const http = require('isomorphic-git/http/node');
+const get = require('simple-get');
+const { PassThrough } = require('stream');
 const { Octokit } = require('@octokit/rest');
 
 const root = process.cwd();
 const ignoreDirs = new Set(['node_modules', 'build', 'dist', 'coverage', '.git']);
+
+function fromValue(value) {
+  let queue = [value];
+  return {
+    next() {
+      return Promise.resolve({ done: queue.length === 0, value: queue.pop() });
+    },
+    return() {
+      queue = [];
+      return {};
+    },
+    [Symbol.asyncIterator]() {
+      return this;
+    },
+  };
+}
+
+function getIterator(iterable) {
+  if (iterable[Symbol.asyncIterator]) return iterable[Symbol.asyncIterator]();
+  if (iterable[Symbol.iterator]) return iterable[Symbol.iterator]();
+  if (iterable.next) return iterable;
+  return fromValue(iterable);
+}
+
+async function forAwait(iterable, cb) {
+  const iter = getIterator(iterable);
+  while (true) {
+    const { value, done } = await iter.next();
+    if (value) await cb(value);
+    if (done) break;
+  }
+  if (iter.return) iter.return();
+}
+
+function asyncIteratorToStream(iter) {
+  const stream = new PassThrough();
+  setTimeout(async () => {
+    await forAwait(iter, chunk => stream.write(chunk));
+    stream.end();
+  }, 1);
+  return stream;
+}
+
+async function collect(iterable) {
+  let size = 0;
+  const buffers = [];
+  await forAwait(iterable, value => {
+    buffers.push(value);
+    size += value.byteLength;
+  });
+  const result = new Uint8Array(size);
+  let nextIndex = 0;
+  for (const buffer of buffers) {
+    result.set(buffer, nextIndex);
+    nextIndex += buffer.byteLength;
+  }
+  return result;
+}
+
+function fromNodeStream(stream) {
+  let ended = false;
+  const queue = [];
+  let defer = {};
+  stream.on('data', chunk => {
+    queue.push(chunk);
+    if (defer.resolve) {
+      defer.resolve({ value: queue.shift(), done: false });
+      defer = {};
+    }
+  });
+  stream.on('error', err => {
+    if (defer.reject) {
+      defer.reject(err);
+      defer = {};
+    }
+  });
+  stream.on('end', () => {
+    ended = true;
+    if (defer.resolve) {
+      defer.resolve({ done: true });
+      defer = {};
+    }
+  });
+  return {
+    next() {
+      return new Promise((resolve, reject) => {
+        if (queue.length === 0 && ended) {
+          return resolve({ done: true });
+        } else if (queue.length > 0) {
+          return resolve({ value: queue.shift(), done: false });
+        } else if (queue.length === 0 && !ended) {
+          defer = { resolve, reject };
+        }
+      });
+    },
+    return() {
+      stream.removeAllListeners();
+      if (stream.destroy) stream.destroy();
+    },
+    [Symbol.asyncIterator]() {
+      return this;
+    },
+  };
+}
+
+const http = {
+  request: async ({ url, method = 'GET', headers = {}, agent, body }) => {
+    if (body && Array.isArray(body)) {
+      body = Buffer.from(await collect(body));
+    } else if (body) {
+      body = asyncIteratorToStream(body);
+    }
+    return new Promise((resolve, reject) => {
+      get(
+        {
+          url,
+          method,
+          headers,
+          agent,
+          body,
+          timeout: 120000,
+        },
+        (err, res) => {
+          if (err) return reject(err);
+          try {
+            const iter = fromNodeStream(res);
+            resolve({
+              url: res.url,
+              method: res.method,
+              statusCode: res.statusCode,
+              statusMessage: res.statusMessage,
+              body: iter,
+              headers: res.headers,
+            });
+          } catch (e) {
+            reject(e);
+          }
+        }
+      );
+    });
+  },
+};
 
 async function listFiles(dir) {
   const result = [];
